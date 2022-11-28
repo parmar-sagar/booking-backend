e48bacc023834a055cac183fbfaa33bbfaefdbc0
sandeep <sandeep@univisionz.com>
2022-11-28 15:37:54 +0530

    frontend design implement

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.layouts.master');
 });
 
 Route::get('/dashboard', function () {
