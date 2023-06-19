7bd66645bffb55117667b2aa6955e3bd9fb3895b
abhishek@univisionz.com <abhishek@univisionz.com>
2023-06-19 13:11:09 +0530

    Update 2022_11_08_042126_create_admins_table.php

--- database/migrations/2022_11_08_042126_create_admins_table.php
+++ database/migrations/2022_11_08_042126_create_admins_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('is_admin',4);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
